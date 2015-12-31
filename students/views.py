6cd8ecd49a2b1b4a8483ef35cf482b04bbe9bec7
Yulia <yulia.scherbina@gmail.com>
2015-12-31 15:28:06 +0100

    add dynamic students list from view

--- students/views.py
+++ students/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
 from django.shortcuts import render
 #from django.http import Http404
 from django.http import HttpResponse
@@ -28,8 +30,46 @@ from django.http import HttpResponse
 
 #views for students
 
+#def students_list(request):
+#    return render(request, 'students-list.html', {})
+
+
+#def render(request, template_name, context=None, content_type=None, status=None, using=None):
+#    """
+#    Returns a HttpResponse whose content is filled with the result of calling
+#    django.template.loader.render_to_string() with the passed arguments.
+#    """
+#    content = loader.render_to_string(template_name, context, request, using=using)
+#    return HttpResponse(content, content_type, status)
+
 def students_list(request):
-    return render(request, 'students-list.html', {})
+    students = (
+        {'id' : 1,
+         'first_name' : 'Julia',
+         'last_name' : 'Dizhak',
+         'ticket' : 1,
+         'image' : 'img/icon-book2.png'
+        },
+        {'id' : 2,
+         'first_name' : 'Vasyl',
+         'last_name' : 'Dizhak',
+         'ticket' : 2,
+         'image' : 'img/define-icon.png'
+        },
+        {'id' : 3,
+          'first_name' : 'test',
+          'last_name' : 'Test',
+          'ticket' : 3,
+          'image' : 'img/icon-book2.png'
+        },
+        {'id' : 4,
+         'first_name' : u'Bacя',
+         'last_name' : 'Run',
+         'ticket' : 4,
+         'image' : 'img/define-icon.png'
+         }
+    )
+    return render(request, 'students-list.html', {'students' : students, 'name': 'test', 'date': datetime.now(), 'year' : datetime.now().year})
 
 def students_add(request):
     return HttpResponse('<h1>Student add form</h1>')
